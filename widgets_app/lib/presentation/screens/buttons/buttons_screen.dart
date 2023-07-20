286296b0d92657f235d12c9555a27d0aa5beb05b
kvzon1984 <ivega.josue@gmail.com>
2023-07-20 14:18:10 -0400

    Buttons Screen Finish

--- widgets_app/lib/presentation/screens/buttons/buttons_screen.dart
+++ widgets_app/lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonsScreen extends StatelessWidget {
   static const String name = 'buttons_screen';
@@ -10,7 +11,104 @@ class ButtonsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Buttons Screens'),
       ),
-      body: const Placeholder(),
+      body: const _ButtonsView(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.arrow_back_ios_new_rounded),
+        onPressed: () {
+          context.pop();
+        }),
+    );
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+  const _ButtonsView();
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: Wrap(
+          spacing: 10,
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(onPressed: () {}, child: const Text('Elevated Button')),
+            const ElevatedButton(onPressed: null, child: Text('Elevated Disable')),
+            ElevatedButton.icon(
+              onPressed: (){},
+              icon: const Icon(Icons.access_alarm_rounded),
+              label: const Text('Elevated Icon'
+            )),
+
+            FilledButton(onPressed: (){},child:  const Text('Filled Button')),
+            FilledButton.icon(
+              onPressed: (){},
+              icon: const Icon(Icons.adb_sharp),
+              label: const Text('Filled Icons'),
+            ),
+
+            OutlinedButton(onPressed: (){}, child: const Text('OutLine Button')),
+            OutlinedButton.icon(
+              onPressed: (){},
+              icon: const Icon(Icons.account_tree),
+              label: const Text('OutLineIcon Button')
+            ),
+
+          TextButton(onPressed: () {}, child: const Text('Text Button')),
+          TextButton.icon(
+            onPressed: () {},
+            label: const Text('Text Button'),
+            icon: const Icon(Icons.terminal),
+          ),
+
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.app_registration_outlined),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.app_registration_outlined),
+            style: ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll(colors.primary),
+              iconColor: const MaterialStatePropertyAll(Colors.white),
+            ),
+          ),
+
+          const CustomButton()
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+    
+        color: colors.primary,
+        child: InkWell(
+          onTap: (){},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text('Custom Button', style: TextStyle(color: Colors.white),),
+          ),
+        ),
+      ),
     );
   }
 }
--- widgets_app/lib/presentation/screens/home/home_screens.dart
+++ widgets_app/lib/presentation/screens/home/home_screens.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 import '../../../config/menu/menu_items.dart';
 
 class HomeScreens extends StatelessWidget {
