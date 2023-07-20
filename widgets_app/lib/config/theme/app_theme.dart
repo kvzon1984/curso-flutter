42b19ecab5bae3ec4972a087d529906ce7f18c58
kvzon1984 <ivega.josue@gmail.com>
2023-07-19 21:56:19 -0400

    add colors theme

---
+++ widgets_app/lib/config/theme/app_theme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.black,
+  Colors.white,
+  Colors.blueGrey,
+  Colors.blue,
+  Colors.amber,
+  Colors.red,
+  Colors.brown,
+  Colors.teal,
+  Colors.green,
+  
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0
+  }): assert(selectedColor >= 0, 'Selected color must be greater then 0'),
+      assert(selectedColor < colorList.length,
+        'Selected color must be less or equal than ${colorList.length - 1}');
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: colorList[ selectedColor ]
+  );
+}
