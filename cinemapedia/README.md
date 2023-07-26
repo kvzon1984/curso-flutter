701738db73616f8bed069210777c8b93a80a4152
kvzon1984 <ivega.josue@gmail.com>
2023-07-25 23:49:21 -0400

    .env cinematica listo

--- cinemapedia/README.md
+++ cinemapedia/README.md
@@ -1,3 +1,9 @@
 # cinemapedia
 
 A new Flutter project.
+
+
+# DEV
+
+1. Copiar el .env.template y recombrarlo a .env
+2. Cambiar las variables de entornos(The MovieDB)
--- cinemapedia/lib/domain/repositories/movies_repositories.dart
+++ cinemapedia/lib/domain/repositories/movies_repositories.dart
@@ -3,5 +3,5 @@ import 'package:cinemapedia/domain/entities/movie_entities.dart';
 abstract class MoviesRepository {
 
   Future<List<Movie>> getNowPlaying({ int page = 1 });
-  
+
 }
